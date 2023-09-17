873f27d86708cc28f44762aff33d7fd24a6238ac
Civileiro <87790842+Civileiro@users.noreply.github.com>
2023-09-17 19:36:03 +0000

    correctly format 0

--- main.py
+++ main.py
@@ -25,13 +25,18 @@ print("(f°g)(x) = ", str_function(fg))
 
 
 def str_float(n):
-  return str(n).rstrip("0").rstrip(".")
+  if n == 0:
+    return "0"
+  else:
+    return str(n).rstrip("0").rstrip(".")
 
 def str_fx(f, x):
   return str_float(f.subs({"x": x}).evalf(chop=True))
 
-for i in range(3):
-  print(f"---TESTE {i + 1}---")
+i = 1
+while True:
+  print(f"---TESTE {i}---")
+  i += 1
   x = float(input("x = "))
   print(f"(g°f)({str_float(x)}) = {str_fx(gf, x)}")
   print(f"(f°f)({str_float(x)}) = {str_fx(ff, x)}")
